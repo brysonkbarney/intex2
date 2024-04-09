1ee6b0cbab354b978f3cad9e393b84b6d71a277d
atrommlitz <119766231+atrommlitz@users.noreply.github.com>
2024-04-08 21:54:10 -0600

    new pages for the site
    best

--- intex2/Controllers/HomeController.cs
+++ intex2/Controllers/HomeController.cs
@@ -22,6 +22,14 @@ public class HomeController : Controller
     {
         return View();
     }
+    public IActionResult Shop()
+    {
+        return View();
+    }
+    public IActionResult AboutUs()
+    {
+        return View();
+    }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
