81776f13edd968c1ee93271a25878baafe1ded9b
atrommlitz <119766231+atrommlitz@users.noreply.github.com>
2024-04-09 09:04:44 -0600

    privacy policy

--- intex2/Program.cs
+++ intex2/Program.cs
@@ -8,11 +8,11 @@ var builder = WebApplication.CreateBuilder(args);
 var services = builder.Services;
 var configuration = builder.Configuration;
 
-services.AddAuthentication().AddGoogle(googleOptions =>
-{
-    googleOptions.ClientId = configuration["Authentication:Google:ClientId"];
-    googleOptions.ClientSecret = configuration["Authentication:Google:ClientSecret"];
-});
+// services.AddAuthentication().AddGoogle(googleOptions =>
+// {
+//     googleOptions.ClientId = configuration["Authentication:Google:ClientId"];
+//     googleOptions.ClientSecret = configuration["Authentication:Google:ClientSecret"];
+// });
 
 builder.Services.AddDbContext<Lego2IntexContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
