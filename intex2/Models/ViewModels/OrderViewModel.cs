91b795197b47c9366aa34eb2409f4886e39763b1
Landon Graham <landonjg41@gmail.com>
2024-04-10 17:32:41 -0600

    adding new views and order view model

---
+++ intex2/Models/ViewModels/OrderViewModel.cs
@@ -0,0 +1,30 @@
+namespace intex2.Models.ViewModels;
+
+public class OrderViewModel
+{
+    public int TransactionId { get; set; }
+
+    public int CustomerId { get; set; }
+
+    public DateOnly? Date { get; set; }
+
+    public string? DayOfWeek { get; set; }
+
+    public byte? Time { get; set; }
+
+    public string? EntryMode { get; set; }
+
+    public int? Amount { get; set; }
+
+    public string? TypeOfTransaction { get; set; }
+
+    public string? CountryOfTransaction { get; set; }
+
+    public string? ShippingAddress { get; set; }
+
+    public string? Bank { get; set; }
+
+    public string? TypeOfCard { get; set; }
+
+    public byte? Fraud { get; set; }
+}
