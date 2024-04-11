9b1af2f7c2bab28f46613d6b19183c4d5d73c7a0
atrommlitz <119766231+atrommlitz@users.noreply.github.com>
2024-04-11 11:51:58 -0600

    rate limit take out

--- intex2/Program.cs
+++ intex2/Program.cs
@@ -6,7 +6,6 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Azure.Security.KeyVault.Secrets;
 using Azure.Identity;
-using AspNetCoreRateLimit;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
